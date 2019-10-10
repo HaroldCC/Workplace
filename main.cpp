7cf790e2bba87a2919f2dc9f0d5b37e3dc3e8292
HaroldCC <49848762+HaroldCC@users.noreply.github.com>
2019-10-10 17:30:59 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,33 @@
-#include <iostream>
+#include<iostream>
+#include<string>
+using namespace std;
 
-//显示输入提示并获取输入信息
+struct pcb
+{
+    string process_name;              //进程名
+    int process_super;                //优先数
+    int need_times;                   //需要运行时间
+    int runded_times;                //已运行时间
+    char[3] process_status = {'W', 'R', 'F'};//进程状态
+};
+
+
 void input(void){
-	
-}
+    int sum;                   //进程总数
+    cout << "输入要执行的进程数量：";
+    cin >> sum;
 
-int main(void) {
-	return 0;
+    //分别读入进程的内容
+    for (int i = 0; i < sum; i++)
+    {
+        cout << endl;
+        cout << "进程号No." << i + 1 << endl;
+        cout << "输入进程名";
+        cin >> process_name;
+        cout << "输入进程优先数：";
+        cin >> process_super;
+        cout << "输入进程运行时间：";
+        cin >> need_times;
+    }
+    
 }
